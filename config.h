b1361a6eaa1c44dfadf1e7ae743db1e12cca17cc
DarkUlixes <reiarifi@gmail.com>
2020-03-24 13:56:09 +0100

    changed color

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#000000" },
-	[SchemeSel] = { "#8BB7C7", "#000000" },
+	[SchemeSel] = { "#9F280A", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
